47c5d46a37e4bb878b58424e051be6070a56d2b2
ledemkam <81742990+ledemkam@users.noreply.github.com>
2024-06-16 07:56:33 +0200

    class contac implement

---
+++ demo/src/main/java/com/example/phonecall/PhoneBook.java
@@ -0,0 +1,36 @@
+package com.example.phonecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class PhoneBook {
+
+	public  static  Scanner sc = null;
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBook.class, args);
+		sc = new Scanner(System.in);
+
+		String userLastName= getUserInput("Geben Sie einen NachNamen ein:  ");
+		String userFirstName= getUserInput("Geben Sie einen Vornamen ein:  ");
+		String userPhoneNumber= getUserInput("Geben Sie eine Telefonnummer ein:  ");
+
+		Contact newContact = new Contact(userLastName, userFirstName, userPhoneNumber);
+
+		System.out.println("Nachname: " + userLastName);
+		System.out.println("Vorname: " + userFirstName);
+		System.out.println("Telefonnummer: " + userPhoneNumber);
+
+		sc.close();
+
+	}
+
+	public static String getUserInput(String userRequest) {
+		System.out.println(userRequest);
+        return sc.nextLine();
+	}
+
+}
